1fb02274e103ba3b970a833f036839ae10543a37
Thorsten Woywod <tw@web-computing.de>
2019-01-11 13:58:44 +0100

    add genericUtterancesInjectionNames

--- src/assistant-generic-utterances.ts
+++ src/assistant-generic-utterances.ts
@@ -1,1 +1,4 @@
 export { descriptor } from "./components/generic-utterances/descriptor";
+export {
+  genericUtterancesInjectionNames
+} from "./components/generic-utterances/injection-names";
---
+++ src/components/generic-utterances/injection-names.ts
@@ -0,0 +1,7 @@
+/** Names of injectionable services, leads to fewer typing errors for most important injections */
+export const genericUtterancesInjectionNames = {
+  /**
+   * Inject an instance of @type {Component<Configuration.Runtime>}
+   */
+  component: "meta:component//generic-utterances"
+};
